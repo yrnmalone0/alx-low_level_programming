c1708491ea3e69b69280ab9ff35aba9026cfc0bc
obed <obedbadu04@gmail.com>
2022-09-08 21:36:13 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
-  * main - Entry point	
-  *
-  * Return: 0
-  */
+ * main - Entry point
+ * function - puts
+ * Return: 0
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-        return (0);
-}
+	return (0);
+}							}
