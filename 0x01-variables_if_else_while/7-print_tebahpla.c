5cfd4e8e127d3eecc4dea8a3f242edd3488c347d
obed <obedbadu04@gmail.com>
2022-09-11 14:59:06 -0700

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ch;
+
+	while (ch = 'z'; ch >= 'a'; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n);
+	return (0);
+}
