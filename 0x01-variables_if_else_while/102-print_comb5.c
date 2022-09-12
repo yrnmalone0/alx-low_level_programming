61a1e4e5e6cbfd32e9030680a69e97e80e0fbeea
obed <obedbadu04@gmail.com>
2022-09-11 18:12:35 -0700

    12. Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -19,7 +19,7 @@ int main(void)
 				putchar(' ');
 				putchar((b / 10) + 48);
 				putchar((b % 10) + 48);
-				if (a != 98 || b !=99)
+				if (a != 98 || b != 99)
 				{
 					putchar(',');
 					putchar(' ');
